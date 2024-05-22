50e2c43ab94c5946c5f714d97f8d1ba3d1ba0a4d
No1Richardo <157613752+No1Richardo@users.noreply.github.com>
2024-05-22 11:54:54 +0100

    models

---
+++ models.go
@@ -0,0 +1,9 @@
+package bigpanda
+
+// Coffee -
+type IncidentTag struct {
+	ID          types.String `tfsdk:"id"`
+	Name        types.String `tfsdk:"name"`
+	Description types.String `tfsdk:"description"`
+	Type        types.String `tfsdk:"type"`
+}
