ff7cca6ad243a2155eb03a1291814035359cd9ad
No1Richardo <157613752+No1Richardo@users.noreply.github.com>
2024-05-22 14:10:28 +0100

    added new incident tags definitions

--- models.go
+++ models.go
@@ -2,8 +2,15 @@ package bigpanda
 
 // IncidentTag -
 type IncidentTag struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Type        string `json:"type"`
+	ID          		string `json:"id"`
+	Name        		string `json:"name"`
+	Description 		string `json:"description"`
+	Type        		string `json:"type"`
+	Active:     		bool `json:"active"`
+	Deleted:    		bool `json:"deleted"`
+	CanManualInput:     bool `json:"canManualInput"`
+	CreatedAt:      	int64 `json:"created_at"`
+	CreatedBy:        	string `json:"created_by"`
+	UpdatedAt:      	int64 `json:"updated_at"`
+	UpdatedBy:        	string `json:"updated_by"`
 }
