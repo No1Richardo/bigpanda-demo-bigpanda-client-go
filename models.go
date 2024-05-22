7cf627f065eb5566b3bac83a58efc9bfe7f7d779
No1Richardo <157613752+No1Richardo@users.noreply.github.com>
2024-05-22 14:13:08 +0100

    updated models

--- models.go
+++ models.go
@@ -6,11 +6,11 @@ type IncidentTag struct {
 	Name        		string `json:"name"`
 	Description 		string `json:"description"`
 	Type        		string `json:"type"`
-	Active:     		bool `json:"active"`
-	Deleted:    		bool `json:"deleted"`
-	CanManualInput:     bool `json:"canManualInput"`
-	CreatedAt:      	int64 `json:"created_at"`
-	CreatedBy:        	string `json:"created_by"`
-	UpdatedAt:      	int64 `json:"updated_at"`
-	UpdatedBy:        	string `json:"updated_by"`
+	Active     			bool `json:"active"`
+	Deleted    			bool `json:"deleted"`
+	CanManualInput     	bool `json:"canManualInput"`
+	CreatedAt      		int64 `json:"created_at"`
+	CreatedBy        	string `json:"created_by"`
+	UpdatedAt      		int64 `json:"updated_at"`
+	UpdatedBy        	string `json:"updated_by"`
 }
